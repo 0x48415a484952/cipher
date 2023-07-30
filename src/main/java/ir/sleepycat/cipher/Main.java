53d47a956b1a9b7777664611b528174425e38736
0x48415a484952 <0x48415a484952@gmail.com>
2023-07-30 12:24:34 +0330

    Remove unused dependencies (#2)
    * Initialize project
    
    * Add gui for encryption and decryption
    
    * Add password field and button styles
    
    * Fix password field
    
    * Add encrypted text to a file button and logic
    
    * Add cli and gui runner
    
    * Remove unused dependencies
    
    * Remove db configs
    
    ---------
    
    Authored-by: Hazhir

--- src/main/java/ir/sleepycat/cipher/Main.java
+++ src/main/java/ir/sleepycat/cipher/Main.java
@@ -1,23 +1,14 @@
 package ir.sleepycat.cipher;
 
 
-import ir.sleepycat.cipher.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import ir.sleepycat.cipher.cli.CommandLine;
+import ir.sleepycat.cipher.gui.GuiApplication;
 
 public class Main {
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        try (Session session = sessionFactory.openSession()) {
-            Transaction tx = session.beginTransaction();
-
-            // Perform CRUD operations using Hibernate
-            // Example: session.save(yourEntity);
-
-            tx.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (args.length > 0 && args[0].equals("--gui")) {
+            GuiApplication.runGui(args);
         }
+        CommandLine.runCli(args);
     }
 }
